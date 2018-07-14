ee474c6f598ac154056610a0ed44bcbee6b4e81c
MongoPong0 <vlorente@alu.ucam.edu>
2018-07-14 13:44:58 +0200

    Day 1: Proyecto creado, primeros toques visuales, color de la app, logo y comienzo de desarrollo de la actividad de login mediante google a firebase.

---
+++ app/src/main/java/android/devs/vlorente/tfg/LoginActivity.kt
@@ -0,0 +1,11 @@
+package android.devs.vlorente.tfg
+
+/**
+ * Created by Valentín Lorente Jiménez on 14/07/2018.
+ * Copyright © 2017 vLorente. All rights reserved.
+ */
+class LoginActivity {
+
+
+
+}
